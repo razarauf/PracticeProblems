a5e16bd92cd49bd0f7fc55c80d21c122e4d2c98a
Raza Rauf <m.razarauf@gmail.com>
2020-02-08 21:59:33 -0500

    popularNToys

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -14,6 +14,24 @@ namespace PracticeProblems
             Console.ReadKey();
         }
 
+        public static List<string> popularNToys(int numToys,
+                                     int topToys,
+                                     List<string> toys,
+                                     int numQuotes, List<string> quotes)
+        {
+            Dictionary<string, int> toyQuoteCount = new Dictionary<string, int>();
+
+            for (int i = 0; i < numToys; i++)
+            {
+                Console.WriteLine(quotes.Where(eachQuote => eachQuote.IndexOf(toys[i], StringComparison.OrdinalIgnoreCase) > 0).Count());
+
+                toyQuoteCount.Add(toys[i], quotes.Where(eachQuote => eachQuote.Contains(toys[i])).Count());
+            }
+
+            var orderedCount = toyQuoteCount.OrderByDescending(x => x.Value).ToList();
+
+            return orderedCount.Select(kvp => kvp.Key).Take(2).ToList();
+        }
 
     }
 }
