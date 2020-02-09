14eb369a1726e9e96dcf2423820cae5a7c94c725
Raza Rauf <m.razarauf@gmail.com>
2020-02-08 21:53:21 -0500

    seprate cellcompete into a class

---
+++ PracticeProblems/CellCompete.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public static class CellCompete
+    {
+        public static void CellCompeteInput()
+        {
+            int[] states = new int[8] { 0, 1, 1, 0, 1, 1, 0, 1 };
+
+            Console.WriteLine(string.Join(",", Main(states, 1)));
+        }
+
+        public static int[] Main(int[] states, int days)
+        {
+            // INSERT YOUR CODE HERE
+            for (int i = 0; i < days; i++)
+            {
+                states = CellCompeteHelper(states, new int[states.Length]);
+            }
+            return states;
+        }
+
+        public static int[] CellCompeteHelper(int[] states, int[] returnedStates)
+        {
+            returnedStates[0] = states[1];
+            returnedStates[7] = states[6];
+
+            for (int i = 1; i < states.Length - 1; i++)
+            {
+                returnedStates[i] = ProcessXOR(
+                    states[i - 1] == 1 ? true : false,
+                    states[i + 1] == 1 ? true : false);
+            }
+
+            return returnedStates;
+        }
+
+        public static int ProcessXOR(bool state1, bool state2)
+        {
+            return state1 ^ state2 == true ? 1 : 0;
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -10,41 +10,10 @@ namespace PracticeProblems
     {
         static void Main(string[] args)
         {
-            int[] states = new int[8] { 0, 1, 1, 0, 1, 1, 0, 1 };
-
-            Console.WriteLine(string.Join(",", cellCompete(states, 1)));
 
             Console.ReadKey();
         }
 
-        public static int[] cellCompete(int[] states, int days)
-        {
-            // INSERT YOUR CODE HERE
-            for (int i = 0; i < days; i++)
-            {
-                states = cellCompeteHelper(states, new int[states.Length]);
-            }
-            return states;
-        }
 
-        public static int[] cellCompeteHelper(int[] states, int[] returnedStates)
-        {
-            returnedStates[0] = states[1];
-            returnedStates[7] = states[6];
-
-            for (int i = 1; i < states.Length - 1; i++)
-            {
-                returnedStates[i] = processXOR(
-                    states[i - 1] == 1 ? true : false,
-                    states[i + 1] == 1 ? true : false);
-            }
-
-            return returnedStates;
-        }
-
-        public static int processXOR(bool state1, bool state2)
-        {
-            return state1 ^ state2 == true ? 1 : 0;
-        }
     }
 }
