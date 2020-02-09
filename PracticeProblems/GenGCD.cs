d508b4805ecc8638d66548d7229e2d5311f39a81
Raza Rauf <m.razarauf@gmail.com>
2020-02-08 21:58:49 -0500

    generalizedGCD

---
+++ PracticeProblems/GenGCD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public static class GenGCD
+    {
+        public static void Main(string[] args)
+        {
+            // { 2, 4, 6, 8, 10}
+            // { 2, 3, 4, 5, 6 }
+            int[] arr = new int[5] { 5, 10, 15, 20, 25 };
+
+            Console.WriteLine(string.Join(",", generalizedGCD(5, arr)));
+
+            Console.ReadKey();
+
+        }
+
+        public static int generalizedGCD(int num, int[] arr)
+        {
+            int gcd = 1;
+            // WRITE YOUR CODE HERE
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] % arr[i] != 0)
+                        break;
+                    if (arr[j] != arr[i])
+                        gcd = arr[i];
+                }
+            }
+            return gcd;
+        }
+    }
+}
